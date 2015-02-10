2766942c216e9e309e65196a21138b8fd52c6136
Daniel Mathis <daniel.t.mathis@gmail.com>
2015-02-09 21:50:58 -0700

    Added some playing around code for user mdoel and view...

---
+++ js/user.js
@@ -0,0 +1,64 @@
+var UserModel = Backbone.Model.extend({
+	defaults: {
+		user: 'name',
+		first_name: '',
+		last_name: '',
+		email: '',
+		birthday: null
+	}
+});
+
+var userModel = new UserModel({
+	user: 'danny',
+	first_name: 'Bob',
+	last_name: 'Mahoney'
+	email: 'Bob@Mahoneys.com',
+	birthday: new Date("May 23, 1988")
+});
+
+var UserView = Backbone.View.extend({
+	
+	tagName: 'div',
+
+	// Save template function
+	userTpl: _.template( $("#user-template").html() ),
+
+	events: {
+		'keypress .user': 'updateUser',
+		'keypress .first_name': 'updateFirstName',
+		'keypress .last_name': 'updateLastName',
+		'keypress .email': 'updateEmail',
+		'keypress .birthday': 'updateBirthday'
+	},
+
+	initialize: function(){
+		this.$el = $("#user");
+	},
+
+	render: function(){
+		this.$el.html( this.userTpl( this.model.attributes ));
+		return this;
+	},
+
+	updateUser: function(){
+
+	},
+
+	updateFirstName: function(){
+
+	},
+
+	updateLastName: function(){
+
+	},
+
+	updateEmail: function(){
+
+	},
+
+	updateBirthday: function(){
+
+	}
+});
+
+var userView = new UserView({ model: userModel });
