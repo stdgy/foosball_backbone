a07eb5fb3ef94bad7d285132b99afac0087d723e
Daniel Mathis <daniel.t.mathis@gmail.com>
2015-02-19 23:54:39 -0700

    Added view for user row in user list.

---
+++ js/views/user_row.js
@@ -0,0 +1,19 @@
+var app = app || {};
+
+app.UserRow = Backbone.View.extend({
+	tagName: 'tr',
+
+	template: null,
+
+	events: {
+
+	},
+
+	initialize: function(){
+
+	},
+
+	render: function(){
+		return this;
+	}
+});
