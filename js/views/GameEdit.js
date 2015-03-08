121c3289e3132f965074c8ce015fcc670457e1fe
Daniel Mathis <daniel.t.mathis@gmail.com>
2015-03-07 23:36:13 -0700

    Added GameEdit view.

---
+++ js/views/GameEdit.js
@@ -0,0 +1,30 @@
+var app = app || {};
+
+app.GameEdit = Backbone.View.extend({
+	tagName: 'div',
+
+	template: _.template($('#game-edit-template').html()),
+
+	events: {
+        '.score click': 'score'
+	},
+
+	initialize: function(){
+        // Render the view
+        this.$body = $("#body");
+        this.$body.html(this.el);
+        this.render();
+	},
+
+	render: function(){
+        this.$el.html( this.template({ data: { users: this.collection } }) );
+        
+		return this;
+	},
+
+    score: function(){
+        // Add score
+
+        // Re-render
+    }
+});
